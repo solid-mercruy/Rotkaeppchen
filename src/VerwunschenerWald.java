a827fd090fb2ce0d27867f4bdef64ea07c137148
Hossein <solid.mercury.2000@gmail.com>
2021-02-22 07:54:25 -0500

    added variables relgarding the pathfinding algorithm

--- src/VerwunschenerWald.java
+++ src/VerwunschenerWald.java
@@ -2,8 +2,11 @@ public abstract class VerwunschenerWald {
     //Attribute:
 
     protected Position position;
-
     protected int damage = 0;
+
+    protected Position nextStep;
+    protected int shortestPath = -1;
+    protected boolean visited = false;
 
     //Constructor:
     public VerwunschenerWald(Position position){
@@ -14,10 +17,18 @@ public abstract class VerwunschenerWald {
     public Position getPosition(){
         return this.position;
     }
-
     public int getDamage(){
         return this.damage;
     }
+
+    public Position getNextStep() { return nextStep; }
+    public int getShortestPath() { return shortestPath; }
+    public boolean isVisited() { return visited; }
+
+    //Setters:
+    public void setNextStep(Position nextStep) { this.nextStep = nextStep; }
+    public void setShortestPath(int shortestPath) { this.shortestPath = shortestPath; }
+    public void setVisited(boolean visited) { this.visited = visited; }
 
     //Method
     public abstract String getName();
